e9b5b0106def46f6560565edf9d958879c86d785
tarturr <tartur.dev@gmail.com>
2025-03-07 07:39:08 +0100

    Added colors to the ColorPalette enum.

--- src/main/scala/fr/tartur/deminer/display/components/ColorPalette.scala
+++ src/main/scala/fr/tartur/deminer/display/components/ColorPalette.scala
@@ -5,3 +5,5 @@ import java.awt.Color
 enum ColorPalette(val color: Color):
   case Blue extends ColorPalette(Color(100, 150, 255))
   case Red extends ColorPalette(Color(255, 100, 100))
+  case Green extends ColorPalette(Color(100, 255, 100))
+  case Purple extends ColorPalette(Color(255, 100, 255))
