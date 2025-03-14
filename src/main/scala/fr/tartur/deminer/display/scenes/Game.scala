5f9c22119550286fe956e0eab8860c7b163dba46
tarturr <tartur.dev@gmail.com>
2025-03-14 16:43:43 +0100

    Used new code features.

--- src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
+++ src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
@@ -1,45 +1,22 @@
 package fr.tartur.deminer.display.scenes
 
-import fr.tartur.deminer.display.components.ColorPalette
-import fr.tartur.deminer.display.game.GameLevel
+import fr.tartur.deminer.display.game.{GameBoxContainer, GameLevel}
 
-import java.awt.{Color, GridLayout}
-import javax.swing.{JButton, JPanel}
-import scala.collection.mutable.ArrayBuffer
-import scala.util.Random
+import java.awt.GridLayout
+import javax.swing.JPanel
 
 class Game(level: GameLevel) extends JPanel:
-  private val size: Int = 15 + ((level.value - 1) / 4.0 * 20).toInt
-  private val boxCount: Int = size * size
-  private val bombCount: Int = (boxCount / 10.0).toInt
-  private val bombPositions: Array[Int] =
-    val random = Random()
-    val positions = ArrayBuffer[Int]()
+  private val width: Int = 15 + ((level.value - 1) / 4.0 * 20).toInt
+  private val area: Int = width * width
 
-    for _ <- 1 to bombCount.toInt do
-      var position = random nextInt boxCount
+  private val bombCount = (area / 10.0).toInt
+  private val boxContainer = GameBoxContainer(this.width)
 
-      while positions contains position do
-        position = random nextInt boxCount
-
-      positions += position
-
-    positions.toArray
-
-  super.setLayout(GridLayout(this.size, this.size))
+  super.setLayout(GridLayout(this.width, this.width))
+  this.boxContainer.plantRandomBombs(this.bombCount)
 
   for
-    y <- 0 until this.size
-    x <- 0 until this.size
+    y <- 0 until this.width
+    x <- 0 until this.width
   do
-    val box = JButton()
-    val position = x + y * this.size
-    var color = ColorPalette.EvenBox.color
-
-    if this.bombPositions contains position then
-      color = Color.RED
-    else if (x + y) % 2 != 0 then
-      color = ColorPalette.OddBox.color
-
-    box.setBackground(color)
-    super.add(box)
+    super.add(this.boxContainer(x, y))
