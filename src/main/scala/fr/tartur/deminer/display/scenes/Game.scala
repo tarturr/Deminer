8bfde69bc220ab372ca6fbb270dad923cbfc30f0
tarturr <tartur.dev@gmail.com>
2025-03-14 07:41:13 +0100

    Added random bomb positions' generation.

--- src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
+++ src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
@@ -3,18 +3,43 @@ package fr.tartur.deminer.display.scenes
 import fr.tartur.deminer.display.components.ColorPalette
 import fr.tartur.deminer.display.game.GameLevel
 
-import java.awt.GridLayout
+import java.awt.{Color, GridLayout}
 import javax.swing.{JButton, JPanel}
+import scala.collection.mutable.ArrayBuffer
+import scala.util.Random
 
 class Game(level: GameLevel) extends JPanel:
-  private val size: Int = (level.value / 4.0 * 30).toInt
+  private val size: Int = 15 + ((level.value - 1) / 4.0 * 20).toInt
+  private val boxCount: Int = size * size
+  private val bombCount: Int = (boxCount / 10.0).toInt
+  private val bombPositions: Array[Int] =
+    val random = Random()
+    val positions = ArrayBuffer[Int]()
+
+    for _ <- 1 to bombCount.toInt do
+      var position = random nextInt boxCount
+
+      while positions contains position do
+        position = random nextInt boxCount
+
+      positions += position
+
+    positions.toArray
 
   super.setLayout(GridLayout(this.size, this.size))
 
   for
-    y <- 0 until size
-    x <- 0 until size
+    y <- 0 until this.size
+    x <- 0 until this.size
   do
     val box = JButton()
-    box.setBackground(if (x + y) % 2 == 0 then ColorPalette.EvenBox.color else ColorPalette.OddBox.color)
+    val position = x + y * this.size
+    var color = ColorPalette.EvenBox.color
+
+    if this.bombPositions contains position then
+      color = Color.RED
+    else if (x + y) % 2 != 0 then
+      color = ColorPalette.OddBox.color
+
+    box.setBackground(color)
     super.add(box)
