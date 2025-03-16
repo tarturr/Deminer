a05bd9ff60ea09042ddd0e48fc93b34f5bf28df6
tarturr <tartur.dev@gmail.com>
2025-03-16 12:51:53 +0100

    Added new icons.

--- src/main/scala/fr/tartur/deminer/display/ImageResources.scala
+++ src/main/scala/fr/tartur/deminer/display/ImageResources.scala
@@ -6,9 +6,13 @@ import javax.imageio.ImageIO
 import javax.swing.ImageIcon
 
 class ImageResources(size: Int):
-  private val images: Array[Image] = (
-    for i <- 1 to 8 yield
-      ImageIO.read(File(s"src/main/resources/icon/$i.png")).getScaledInstance(size, size, Image.SCALE_DEFAULT)
-    ).toArray
+  private val numbers: Array[ImageIcon] = (for i <- 1 to 8 yield this.icon(i.toString)).toArray
+  val flag: ImageIcon = this.icon("flag")
+  val bomb: ImageIcon = this.icon("bomb")
 
-  def apply(n: Int): ImageIcon = if n > 0 then ImageIcon(this.images(n - 1)) else null
+  def apply(n: Int): ImageIcon = if n > 0 then this.numbers(n - 1) else null
+
+  private def icon(name: String) = ImageIcon(
+    ImageIO.read(File(s"src/main/resources/icon/$name.png"))
+      .getScaledInstance(size, size, Image.SCALE_DEFAULT)
+  )
