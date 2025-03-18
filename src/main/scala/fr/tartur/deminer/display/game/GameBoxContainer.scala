036a57e86315f630e4eacd0e356a7f6eba3e5088
tarturr <tartur.dev@gmail.com>
2025-03-18 07:07:19 +0100

    Transferred some responsibilities from the GameBoxContainer class to the Game class.

--- src/main/scala/fr/tartur/deminer/display/game/GameBoxContainer.scala
+++ src/main/scala/fr/tartur/deminer/display/game/GameBoxContainer.scala
@@ -3,10 +3,9 @@ package fr.tartur.deminer.display.game
 import fr.tartur.deminer.display.ImageResources
 import fr.tartur.deminer.display.components.{BasicBox, BombBox, GameBox}
 
-import java.beans.{PropertyChangeEvent, PropertyChangeListener}
 import scala.util.Random
 
-class GameBoxContainer(private val width: Int) extends PropertyChangeListener:
+class GameBoxContainer(private val width: Int):
   private val images = ImageResources(32)
   private val area = width * width
   private val container = (
@@ -15,7 +14,6 @@ class GameBoxContainer(private val width: Int) extends PropertyChangeListener:
       x <- 0 until this.width
     yield
       val box = BasicBox(x, y, images)
-      box.addDiscoverListener(this)
       box
   ).toArray[GameBox]
 
@@ -30,14 +28,7 @@ class GameBoxContainer(private val width: Int) extends PropertyChangeListener:
 
       this.plant(coords._1, coords._2)
 
-  private def plant(x: Int, y: Int): Unit =
-    val bomb = BombBox(x, y, images)
-    bomb.addDiscoverListener(this)
-    this.container(this.toIndex(x, y)) = bomb
-
-    this.neighbors(x, y).foreach(box => box.bombsAround += 1)
-
-  private def neighbors(x: Int, y: Int): Array[BasicBox] = (
+  def neighbors(x: Int, y: Int): Array[BasicBox] = (
     for
       y <- y - 1 to y + 1
       x <- x - 1 to x + 1
@@ -46,33 +37,21 @@ class GameBoxContainer(private val width: Int) extends PropertyChangeListener:
       this(x, y).asInstanceOf[BasicBox]
     ).toArray
 
-  private def exists(x: Int, y: Int): Boolean =
-    val range = 0 until this.width
-    range.contains(x) && range.contains(y)
-
   def apply(x: Int, y: Int): GameBox =
     if !this.exists(x, y) then
       throw IndexOutOfBoundsException("Trying to reach a game box at an out of bounds position")
 
     this.container(this.toIndex(x, y))
 
+  private def plant(x: Int, y: Int): Unit =
+    val bomb = BombBox(x, y, images)
+    this.container(this.toIndex(x, y)) = bomb
+
+    this.neighbors(x, y).foreach(box => box.bombsAround += 1)
+
+  private def exists(x: Int, y: Int): Boolean =
+    val range = 0 until this.width
+    range.contains(x) && range.contains(y)
+
   private def toIndex(x: Int, y: Int): Int = y * this.width + x
   private def randomCoords(seed: Random): (Int, Int) = (seed nextInt this.width, seed nextInt this.width)
-
-  private def recursiveDiscover(basicBox: BasicBox): Unit =
-    basicBox.discover()
-
-    if basicBox.bombsAround == 0 then
-      this.neighbors(basicBox.cellX, basicBox.cellY).foreach(box => if !box.isDiscovered then this.recursiveDiscover(box))
-
-  override def propertyChange(event: PropertyChangeEvent): Unit =
-    val operation = event.getPropertyName
-
-    if !operation.equals("discovered") then
-      throw UnsupportedOperationException(s"Unsupported operation: $operation")
-
-    val box = event.getNewValue.asInstanceOf[GameBox]
-
-    box match
-      case basic: BasicBox => this.recursiveDiscover(basic)
-      case _ => ???
--- src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
+++ src/main/scala/fr/tartur/deminer/display/scenes/Game.scala
@@ -1,11 +1,13 @@
 package fr.tartur.deminer.display.scenes
 
+import fr.tartur.deminer.display.components.{BasicBox, GameBox}
 import fr.tartur.deminer.display.game.{GameBoxContainer, GameLevel}
 
 import java.awt.GridLayout
+import java.beans.{PropertyChangeEvent, PropertyChangeListener}
 import javax.swing.JPanel
 
-class Game(level: GameLevel) extends JPanel:
+class Game(level: GameLevel) extends JPanel, PropertyChangeListener:
   private val width: Int = 15 + ((level.value - 1) / 4.0 * 20).toInt
   private val area: Int = width * width
 
@@ -19,4 +21,24 @@ class Game(level: GameLevel) extends JPanel:
     y <- 0 until this.width
     x <- 0 until this.width
   do
-    super.add(this.boxContainer(x, y))
+    val box: GameBox = this.boxContainer(x, y)
+    box.addDiscoverListener(this)
+    super.add(box)
+
+  private def recursiveDiscover(basicBox: BasicBox): Unit =
+    basicBox.discover()
+
+    if basicBox.bombsAround == 0 then
+      this.boxContainer.neighbors(basicBox.cellX, basicBox.cellY).foreach(box => if !box.isDiscovered then this.recursiveDiscover(box))
+
+  override def propertyChange(event: PropertyChangeEvent): Unit =
+    val operation = event.getPropertyName
+
+    if !operation.equals("discovered") then
+      throw UnsupportedOperationException(s"Unsupported operation: $operation")
+
+    val box = event.getNewValue.asInstanceOf[GameBox]
+
+    box match
+      case basic: BasicBox => this.recursiveDiscover(basic)
+      case _ => ???
