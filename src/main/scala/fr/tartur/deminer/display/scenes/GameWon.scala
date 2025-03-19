383ff938c96f389eaf1a426611b67081c9b05b65
tarturr <tartur.dev@gmail.com>
2025-03-19 09:31:44 +0100

    Ended the "game won" screen.

--- src/main/scala/fr/tartur/deminer/display/scenes/GameWon.scala
+++ src/main/scala/fr/tartur/deminer/display/scenes/GameWon.scala
@@ -1,13 +1,23 @@
 package fr.tartur.deminer.display.scenes
 
 import fr.tartur.deminer.display.SceneHolder
-import fr.tartur.deminer.display.components.PlayButton
+import fr.tartur.deminer.display.components.{PlayButton, Title}
 
 import java.awt.GridLayout
-import javax.swing.{Box, JLabel, JPanel}
+import javax.swing.{Box, JPanel}
 
-class GameWon(seconds: Int, holder: SceneHolder) extends JPanel(GridLayout(4, 1, 0, 20)):
-  super.add(Box.createVerticalStrut(50))
-  super.add(JLabel(s"Bravo, vous avez gagné en $seconds !"))
-  super.add(PlayButton("Rejouer", holder))
-  super.add(Box.createVerticalStrut(50))
+class GameWon(seconds: Int, holder: SceneHolder) extends AbstractMenu(
+  Title(s"Bravo, vous avez gagné en $seconds secondes !"),
+  {
+    val body = JPanel(GridLayout(2, 1))
+
+    val button = JPanel(GridLayout(1, 3))
+    button.add(Box.createHorizontalGlue())
+    button.add(PlayButton("Rejouer", holder))
+    button.add(Box.createHorizontalGlue())
+
+    body.add(button)
+    body.add(Box.createHorizontalGlue())
+    body
+  }
+)
